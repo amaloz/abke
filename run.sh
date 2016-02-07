1ac610e368fcfdc96bd19533c04d754aec47aee2
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-02-06 19:34:23 -0500

    script for running experiments

---
+++ run.sh
@@ -0,0 +1,17 @@
+NTIMES=2
+
+mkdir -p logs
+
+for M in 10 100 1000
+do
+
+    for NLAYERS in 1000 10000 100000
+    do
+        echo $M $NLAYERS
+        ./a.out --ca -m $M -t $NTIMES 2>/dev/null &
+        ./a.out --server -m $M -q $NLAYERS -t $NTIMES 2>logs/server.$M.$NLAYERS.log &
+        ./a.out --client -m $M -q $NLAYERS -t $NTIMES 2>logs/client.$M.$NLAYERS.log
+    done
+done
+
+
